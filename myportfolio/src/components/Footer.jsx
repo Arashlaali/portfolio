8a765ead73c0dab1a36e98f2d04376f54a4c7f21
Arash Laali <arashlaali@Arashs-MacBook-Pro.local>
2025-05-01 19:39:08 +0330

    added code for footer

--- myportfolio/src/components/Footer.jsx
+++ myportfolio/src/components/Footer.jsx
@@ -1,7 +1,41 @@
-import "./Footer.css"
+import { useState } from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
-    return (<footer className="footer" > &copy; 2025 - ARASH LAALI </footer>)
+    const [isFooterOpen, setIsFooterOpen] = useState(false); // State to toggle the footer menu
+
+    const toggleFooter = () => {
+        setIsFooterOpen(!isFooterOpen); // Toggle footer visibility
+    };
+
+    return (
+        <footer className="footer">
+            <h2>ARASH LAALI</h2>
+
+            {/* Toggle Icon */}
+            <div className="footer-toggle" onClick={toggleFooter}>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
+
+            {/* Footer Links */}
+            <ul className={`footer-ul ${isFooterOpen ? 'active' : ''}`}>
+                <li>
+                    <Link to="/contact">CONTACT</Link>
+                </li>
+                <li>
+                    <Link to="/privacy">PRIVACY POLICY</Link>
+                </li>
+                <li>
+                    <Link to="/terms">TERMS OF SERVICE</Link>
+                </li>
+                <li>
+                    <Link to="/socials">SOCIAL MEDIA</Link>
+                </li>
+            </ul>
+        </footer>
+    );
 }
 
 export default Footer;
